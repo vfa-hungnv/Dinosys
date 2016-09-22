ed59678513291dc863a4961224d380a16607d573
vfa-hungnv <donmichael1Root@donmichaels-macbook-pro.local>
2016-09-22 15:18:37 +0700

    Initial Commit

---
+++ Dinosys_Corp_News/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Dinosys_Corp_News
+//
+//  Created by Hung Nguyen on 9/22/16.
+//  Copyright © 2016 Hung Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
